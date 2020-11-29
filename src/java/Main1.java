c75713de740d7cccbdc56ca2cbdcb3b3448e1892
zyj929 <zhangyongjian929@163.com>
2020-11-29 14:12:45 +0800

    初始化代码

---
+++ src/java/Main1.java
@@ -0,0 +1,12 @@
+/**
+ * @autor zyj
+ * @date 2020/11/29 14:05
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("初始化代码1");
+        System.out.println("初始化代码2");
+        System.out.println("初始化代码3");
+        System.out.println("初始化代码4");
+    }
+}
